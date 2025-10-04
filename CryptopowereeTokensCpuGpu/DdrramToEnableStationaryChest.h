5811bd2f4e21dfaa37e353ee5ee661d81b60b1cb
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2025-10-04 10:57:07 +0200

    Update DdrramToEnableStationaryChest.h

--- CryptopowereeTokensCpuGpu/DdrramToEnableStationaryChest.h
+++ CryptopowereeTokensCpuGpu/DdrramToEnableStationaryChest.h
@@ -6,6 +6,40 @@
 //so once again, we need stackd. thr', this id a perfect behaviour
 //for stationnary ghedt.
 
+#ifndef CRYPTOPOWEREE_TOKENS_CPU_GPU_DDRRAM_ENABLE_STATIONARY_CHEST_H
+#define CRYPTOPOWEREE_TOKENS_CPU_GPU_DDRRAM_ENABLE_STATIONARY_CHEST_H
+
+//we must use cintainers 
+
+
+//there is a way to corrupt iso datas.
+//but as containers in ram are flushed, we can prevent maintaining hd.
+//so once again, we need stackd. thr', this id a perfect behaviour
+//for stationnary ghedt.
+
+
+
+
+//as stacks mustnt directly communicate with gpus, and ad gpus architectue could be old,
+//we gan send signals to hd. this is corrupted.
+//to prevent it, letd use more isos. then isos must be created by gpus.
+
+namespace chest {
+
+class DdrRamStationaryChest {
+public:
+  void enable();
+  void disable();
+
+  //old configure. mass transfer and heat transfer may be a common behaviour.
+  //then, we can introduce.new behaviour. this can lead
+  //to resolve senderd. so, the amount of signals are clamped to 6*10^9 (high school maths).
+  void sendSignalsPla'ckToOldDdrram();
+};
+
+} // namespace chest
+
+#endif // CRYPTOPOWEREE_TOKENS_CPU_GPU_DDRRAM_ENABLE_STATIONARY_CHEST_H
 
 
 
