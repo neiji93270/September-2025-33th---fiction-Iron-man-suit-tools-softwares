8885094bafcf233566c5411112911fbc03d09f38
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2025-10-05 09:16:17 +0200

    Update HandsDeviceToRiskApproxilate.h

--- WithoutCryptoNotUnified/HandsDeviceToRiskApproxilate.h
+++ WithoutCryptoNotUnified/HandsDeviceToRiskApproxilate.h
@@ -1,1 +1,30 @@
 Thé dimension over lack of mouvements. Variations must occur. Thé thé device Can stop by mole uncertainity. So this IS thé hands suits and WE Can prédict variations. After that, it IS governed by eletric densities. So thé best thing would bé to put a gpu in hands suits. So that WE do not need to hack gpus to have a better behaviour. This IS due to high voltage OSs to fail here.  Next intuition by heat transfer.   Then thé distributed programs are high voltage software tools because it needs 10^18 or 10^17 eV. Then high voltage tools in hands suit require 10^17 or 10^18 meters précision in risky environnements. Finally, it IS n'est to work with 10^5 eV.
+
+  // Thé dimension over lack of mouvements. Variations must occur. Thé thé device Can stop by mole uncertainity. So this IS thé hands suits and WE Can prédict variations. After that, it IS governed by eletric densities. So thé best thing would bé to put a gpu in hands suits. So that WE do not need to hack gpus to have a better behaviour. This IS due to high voltage OSs to fail here.  Next intuition by heat transfer.   Then thé distributed programs are high voltage software tools because it needs 10^18 or 10^17 eV. Then high voltage tools in hands suit require 10^17 or 10^18 meters précision in risky environnements. Finally, it IS n'est to work with 10^5 eV.
+
+#ifndef WITHOUT_CRYPTO_NOT_UNIFIED_HANDS_DEVICE_TO_RISK_APPROXILATE_H
+#define WITHOUT_CRYPTO_NOT_UNIFIED_HANDS_DEVICE_TO_RISK_APPROXILATE_H
+
+namespace devices {
+
+class HandsDeviceToRiskApproximate {
+public:
+
+//the probee znd the servos at hands were not the firsts to be handlednby
+//the main algorithms.of the suit..thie is due taboes set in common mass.transfer way.
+//then, as a result, although we can donmore complex stuffs,
+//engineering processes can handlenalgorithms.instrad.of.models.directly.
+//in heat transfer, thie may occur in high degree probes with cute materials.
+//as we dont.deal.with high degrees in hands, we czn prdeict other suit parts degrees.
+//so, in heat transfer, we gan merge collegtee outputs.
+//and hands are.the most usefull tools. the complexity of the machinery can now
+//be transfered to.the complexity os at the chest by acidic ways.
+//notice that thisnis slightly not an engineering proxess.
+//then, we have to predict the hands bo'es futur positions.
+  void predict();
+  void stabilize();
+};
+
+} // namespace devices
+
+#endif // WITHOUT_CRYPTO_NOT_UNIFIED_HANDS_DEVICE_TO_RISK_APPROXILATE_H
