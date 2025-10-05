1c5e672467993262f9690cd0fe785c9e7acafdd7
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2025-10-05 06:47:17 +0200

    Update MoveToListofErrors.h

--- CybersecurityforironmanWithRisksmanagelent/MoveToListofErrors.h
+++ CybersecurityforironmanWithRisksmanagelent/MoveToListofErrors.h
@@ -1,3 +1,28 @@
 //we rely on : CryptopowereeTokensCpuGpu/CryptoPoweredAllProbesRz'geInEachPartsHeaderAndSaveItInADatabaseAndInOsProcess.h
 //and we need patching. on the other hand, this file
 //is not suited for all parts of the suit.
+
+
+#ifndef CYBERSECURITY_MOVE_TO_LIST_OF_ERRORS_H
+#define CYBERSECURITY_MOVE_TO_LIST_OF_ERRORS_H
+
+//we rely on : CryptopowereeTokensCpuGpu/CryptoPoweredAllProbesRz'geInEachPartsHeaderAndSaveItInADatabaseAndInOsProcess.h
+//and we need patching. on the other hand, this file
+//is not suited for all parts of the suit.
+
+namespace cyber {
+
+class MoveToListOfErrors {
+public:
+
+ //collecting errors with signals may be hard.
+ //then, in this function, wa can add patched results.
+ //but we wont have quantified results. so, it is better 
+ //to use abelian groups. plus, chaotic systems are not suited here.
+  void collect();
+  void flush();
+};
+
+} // namespace cyber
+
+#endif // CYBERSECURITY_MOVE_TO_LIST_OF_ERRORS_H
